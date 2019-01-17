115113297bafb130c686b1d02ac3868fb002f431
thprennes7 <46448172+thprennes7@users.noreply.github.com>
2019-01-17 16:05:20 +0100

    Delete cryto_scrapper.rb

--- lib/cryto_scrapper.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
-   
-crypto = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))   
-print crypto.xpath('//*[@id="currencies-all"]/tbody').text   # => Nokogiri::HTML::Document
