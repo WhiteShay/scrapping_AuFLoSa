664dd75e15e5ba587703416b66310118537d8e49
Samkttn <kettani.samir@outlook.fr>
2019-01-17 18:20:41 +0100

    BITE!

---
+++ lib/road_of_deputy.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+def get_mail
+    deputy=[]
+    
+
+    page = Nokogiri::HTML(open('https://www.nosdeputes.fr/deputes'))
+    page.xpath('//div[@class="list_table"]//a').each do |row|
+        deputy_page = Nokogiri::HTML(open('https://www.nosdeputes.fr'+row['href']))
+        deputy_identity = deputy_page.xpath('//div[@class="info_depute"]/h1').text.split
+        first_name = deputy_identity[0]
+        last_name = deputy_identity[1]
+        mail = deputy_page.xpath('//a[contains(@href, "@assemblee-nationale.fr")]').text
+        deputy.push([["first_name", first_name], ["last_name", last_name], ["mail", mail]])
+
+        
+    end
+    puts deputy
+end
+
+get_mail
