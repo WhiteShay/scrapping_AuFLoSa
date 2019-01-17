444782ea99fe9eca5ffbd01795a399c7d0ee10c5
aurelien <duquenneaurelien@gmail.com>
2019-01-17 19:27:15 +0100

    finish

--- lib/road_of_deputy.rb
+++ lib/road_of_deputy.rb
@@ -1,23 +1,22 @@
+# frozen_string_literal: true
+
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
 def get_mail
-    deputy=[]
-    
+  deputy = []
 
-    page = Nokogiri::HTML(open('https://www.nosdeputes.fr/deputes'))
-    page.xpath('//div[@class="list_table"]//a').each do |row|
-        deputy_page = Nokogiri::HTML(open('https://www.nosdeputes.fr'+row['href']))
-        deputy_identity = deputy_page.xpath('//div[@class="info_depute"]/h1').text.split
-        first_name = deputy_identity[0]
-        last_name = deputy_identity[1]
-        mail = deputy_page.xpath('//a[contains(@href, "@assemblee-nationale.fr")]').text
-        deputy.push([["first_name", first_name], ["last_name", last_name], ["mail", mail]])
-
-        
-    end
-    puts deputy
+  page = Nokogiri::HTML(open('https://www.nosdeputes.fr/deputes'))
+  page.xpath('//div[@class="list_table"]//a').each do |row|
+    deputy_page = Nokogiri::HTML(open('https://www.nosdeputes.fr' + row['href']))
+    deputy_identity = deputy_page.xpath('//div[@class="info_depute"]/h1').text.split
+    first_name = deputy_identity[0]
+    last_name = deputy_identity[1]
+    mail = deputy_page.xpath('//a[contains(@href, "@assemblee-nationale.fr")]').text
+    deputy.push([['first_name', first_name], ['last_name', last_name], ['mail', mail]])
+  end
+  print deputy
 end
 
 get_mail
--- lib/road_town_hall.rb
+++ lib/road_town_hall.rb
@@ -4,18 +4,17 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-
 def get_mail
-    city_names =[]
-    mail = []
-    page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
-    
-    page.xpath('//a[@class="lientxt"]').each do |row|
-        city_names.push(row.text)
-        mail.push(Nokogiri::HTML(open('http://annuaire-des-mairies.com'+row['href'].sub(".",""))).xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text)
-    end
-    mail_list = city_names.zip(mail)
-    puts mail_list
-    mail_list
+  city_names = []
+  mail = []
+  page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
+
+  page.xpath('//a[@class="lientxt"]').each do |row|
+    city_names.push(row.text)
+    mail.push(Nokogiri::HTML(open('http://annuaire-des-mairies.com' + row['href'].sub('.', ''))).xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text)
+  end
+  mail_list = city_names.zip(mail)
+  puts mail_list
+  mail_list
 end
 get_mail
